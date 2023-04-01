3d66098d43df87c940099fd41778f7143a9429e5
Yogesh Bhatt <ygshbht@gmail.com>
2023-04-01 07:23:42 +0530

    Work on season 1

---
+++ src/components/Season1.tsx
@@ -0,0 +1,91 @@
+import { Box, Dialog, Button, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+import React from "react";
+
+export default function Season1() {
+	return (
+		<Box>
+			<Stack p={5} gap={3} direction="row">
+				<Image
+					style={{ objectFit: "contain" }}
+					width={611}
+					height={509}
+					alt="a of spades"
+					src="/a of spades moth.jpg"
+				></Image>
+				<Box>
+					<Typography>
+						S1 focuses on highlighting emerging artists just entering the crypto
+						space. Game of Chance is a curation of artworks from talented
+						artists who deserves more attention.
+					</Typography>
+					<Stack direction="row">
+						<Box sx={{ width: "50px" }}>
+							<Image
+								style={{
+									position: "relative",
+									left: "-80px",
+									objectFit: "contain",
+								}}
+								width={275}
+								height={234}
+								alt="a of spades"
+								src="/fly path.svg"
+							></Image>
+						</Box>
+						<Typography>Scroll down to find out more.</Typography>
+					</Stack>
+
+					<Button>Mint</Button>
+				</Box>
+			</Stack>
+
+			<Stack alignItems={"center"} direction="row" gap={3}>
+				<Image
+					src="/7 of spades.svg"
+					width={237}
+					height={352}
+					alt="card"
+				></Image>
+				<Image
+					src="/joker of hearts.svg"
+					width={237}
+					height={352}
+					alt="card"
+				></Image>
+				<Typography variant="h1">=</Typography>
+				<Image src="/moth man.jpg" width={486} height={486} alt="card"></Image>
+			</Stack>
+			<Box mt={5}>
+				<Typography variant="h4">Rules:</Typography>
+				<Typography>
+					- Each Moth Valley Pass holder will be airdropped 2 random cards.
+				</Typography>
+				<Typography>
+					- The time will come when you&apos;ll have to burn 2 cards.{" "}
+				</Typography>
+				<Typography>
+					- Each art piece is tied to an exact combination of cards.{" "}
+				</Typography>
+				<Typography>
+					- The art pieces and their respective artists will only be revealed
+					after the burn period.{" "}
+				</Typography>
+				<Typography>
+					- Ignore the suits, only face value plays a role in the outcomes
+				</Typography>
+			</Box>
+			<Box my={2} sx={{ p: 2, border: "2px solid black" }}>
+				{" "}
+				<Typography>For example: </Typography>
+				<Typography>
+					burning A♥️ & 5♠️ will give you an art work from Artist A; and{" "}
+				</Typography>
+				<Typography>
+					burning 8♣️ & K♦️ will give you an art work from Artist B, etc.{" "}
+				</Typography>
+			</Box>
+			<Button sx={{ display: "block", m: "auto" }}>Mint</Button>
+		</Box>
+	);
+}
--- src/components/Seasons.tsx
+++ src/components/Seasons.tsx
@@ -1,6 +1,14 @@
-import { Stack, Box, Dialog, Divider, Typography } from "@mui/material";
-import React from "react";
+import {
+	Stack,
+	Box,
+	Dialog,
+	Divider,
+	Typography,
+	IconButton,
+} from "@mui/material";
+import React, { useState } from "react";
 import Image from "next/image";
+import Season1 from "./Season1";
 
 export default function Seasons({
 	open,
@@ -9,54 +17,78 @@ export default function Seasons({
 	open: boolean;
 	onClose: () => any;
 }) {
+	const [season1Open, setSeason1Open] = useState(false);
 	return (
 		<Dialog maxWidth={false} open={open} onClose={onClose}>
-			<Box sx={{ width: "800px", height: "600px", position: "relative" }} p={5}>
-				<Image
-					onClick={onClose}
-					style={{ position: "absolute", top: 50, right: 50 }}
-					width={50}
-					height={50}
-					src="/close.svg"
-					alt="Close btn"
-				></Image>
+			<Box
+				sx={{ width: "1200px", height: "800px", position: "relative" }}
+				p={5}
+			>
+				<Box sx={{ position: "absolute", top: 50, right: 50 }}>
+					{season1Open ? (
+						<IconButton onClick={() => setSeason1Open(false)}>
+							<Image
+								width={50}
+								height={50}
+								src="/close.svg"
+								alt="Close btn"
+							></Image>
+						</IconButton>
+					) : (
+						<IconButton onClick={onClose}>
+							<Image
+								width={50}
+								height={50}
+								src="/close.svg"
+								alt="Close btn"
+							></Image>
+						</IconButton>
+					)}
+				</Box>
+
 				<Typography variant="h1">Seasons</Typography>
 				<Divider></Divider>
-				<Stack
-					gap={2}
-					p={3}
-					justifyContent={"center"}
-					margin={"auto"}
-					direction="row"
-				>
-					<Stack alignItems="center">
-						<Image
-							width={150}
-							height={150}
-							src="/Folder.svg"
-							alt="Close btn"
-						></Image>
-						<Typography>Season 1</Typography>
+				{/* home */}
+				{season1Open ? (
+					<Season1 />
+				) : (
+					<Stack
+						gap={2}
+						p={3}
+						justifyContent={"center"}
+						margin={"auto"}
+						direction="row"
+					>
+						<Stack alignItems="center">
+							<Image
+								width={150}
+								height={150}
+								onClick={() => setSeason1Open(true)}
+								src="/Folder.svg"
+								alt="Close btn"
+							></Image>
+							<Typography>Season 1</Typography>
+						</Stack>
+						<Stack alignItems="center">
+							<Image
+								width={150}
+								height={150}
+								src="/FolderLock.svg"
+								alt="Close btn"
+							></Image>
+							<Typography>Season 2</Typography>
+						</Stack>
+						<Stack alignItems="center">
+							<Image
+								width={150}
+								height={150}
+								src="/FolderLock.svg"
+								alt="Close btn"
+							></Image>
+							<Typography>Season 3</Typography>
+						</Stack>
 					</Stack>
-					<Stack alignItems="center">
-						<Image
-							width={150}
-							height={150}
-							src="/FolderLock.svg"
-							alt="Close btn"
-						></Image>
-						<Typography>Season 2</Typography>
-					</Stack>
-					<Stack alignItems="center">
-						<Image
-							width={150}
-							height={150}
-							src="/FolderLock.svg"
-							alt="Close btn"
-						></Image>
-						<Typography>Season 3</Typography>
-					</Stack>
-				</Stack>
+				)}
 			</Box>
 		</Dialog>
 	);
