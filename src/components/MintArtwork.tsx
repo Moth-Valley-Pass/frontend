9f45c9b5036c34e8685cd0fcc993c19e75cce0ee
Yogesh Bhatt <ygshbht@gmail.com>
2023-04-03 05:28:37 +0530

    Mint artwork responsiveness

--- src/components/MintArtwork.tsx
+++ src/components/MintArtwork.tsx
@@ -6,12 +6,12 @@ import React, { useState } from "react";
 export default function MintArtwork() {
 	const [minted, setMinted] = useState(false);
 	return (
-		<Box>
+		<Box p={1}>
 			<Stack
 				justifyContent={"space-between"}
-				alignItems={"start"}
+				alignItems={{ xs: "center", lg: "start" }}
 				gap={5}
-				direction="row"
+				direction={{ xs: "column", lg: "row" }}
 			>
 				<Image
 					alt="card fly"
@@ -77,17 +77,30 @@ export default function MintArtwork() {
 }
 export function MintArtWorkHeading() {
 	return (
-		<Stack alignItems="center" gap={5} direction="row">
-			<Image
-				style={{ objectFit: "contain" }}
-				width={100}
-				height={100}
-				alt="a of spades"
-				src="/Folder.svg"
-			></Image>
-			<Typography fontSize="70px" fontWeight="bold" variant="h1">
-				<i> Season 1:</i> Mint Artwork
-			</Typography>
-		</Stack>
+		<Box sx={{ maxWidth: "100%", overflow: "auto" }}>
+			<Stack
+				sx={{
+					img: {
+						width: { xs: 40, md: 100 },
+						height: { xs: 40, md: 100 },
+					},
+					width: "max-content",
+				}}
+				alignItems="center"
+				gap={{ xs: 2, md: 5 }}
+				direction="row"
+			>
+				<Image
+					style={{ objectFit: "contain" }}
+					width={100}
+					height={100}
+					alt="a of spades"
+					src="/Folder.svg"
+				></Image>
+				<Typography fontWeight="bold" variant="h1">
+					<i> Season 1:</i> Mint Artwork
+				</Typography>
+			</Stack>
+		</Box>
 	);
 }
