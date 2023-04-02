da02758988dcf084a731638c5d571a8d11f9652b
Yogesh Bhatt <ygshbht@gmail.com>
2023-04-02 15:25:44 +0530

    update testnets

--- src/CONSTANTS.ts
+++ src/CONSTANTS.ts
@@ -1,5 +1,6 @@
 export const ALLOW_DRAGGING = true;
 export const AVAX_TESTNET = "https://api.avax-test.network/ext/bc/C/rpc";
+export const OWNER_EMAIL = "email@email.com";
 
 // export const CONTRACT_ADDR = "....";
 // export const NETWORK_ID = "....";
@@ -7,9 +8,15 @@ export const AVAX_TESTNET = "https://api.avax-test.network/ext/bc/C/rpc";
 // export const EXPLORER_URI = ".....";
 // export const NETWORK_NAME = "...";
 
-export const CONTRACT_ADDR = "0x8057dcc9db4d1f6011bb19e695862be021d38686";
-export const NETWORK_NAME = "CANTO";
-export const NETWORK_ID = 7700;
-export const RPC_PROVIDER = "https://mainnode.plexnode.org:8545";
-export const EXPLORER_URI = "https://evm.explorer.canto.io";
-export const OWNER_EMAIL = "email@email.com";
+// export const CONTRACT_ADDR = "0x8057dcc9db4d1f6011bb19e695862be021d38686";
+// export const NETWORK_NAME = "CANTO";
+// export const NETWORK_ID = 7700;
+// export const RPC_PROVIDER = "https://mainnode.plexnode.org:8545";
+// export const EXPLORER_URI = "https://evm.explorer.canto.io";
+
+//AVAX TESTNET
+export const CONTRACT_ADDR = "0x5a22323A65CA245478cC6fff3e0035E242036310";
+export const NETWORK_NAME = "AVAX TESTNET";
+export const NETWORK_ID = 43113;
+export const RPC_PROVIDER = "https://api.avax-test.network/ext/bc/C/rpc";
+export const EXPLORER_URI = "https://testnet.snowtrace.io/";
